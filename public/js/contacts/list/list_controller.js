94447c4f913bd04b4fe1f3b85d7b3f6d6227aa9b
Dmitry Groza <boxfrommars@gmail.com>
2013-12-09 04:02:48 +0400

    loading state

--- public/js/contacts/list/list_controller.js
+++ public/js/contacts/list/list_controller.js
@@ -2,6 +2,9 @@ App.module('ContactsApp.List', function(List, App){
     List.Controller = {
         listContacts: function(){
 
+            var loadingView = new App.Common.Views.Loading();
+            App.mainRegion.show(loadingView);
+            
             var fetchingContacts = App.request('contact:entities');
 
             $.when(fetchingContacts).done(function(contacts){
